f6bd5121ee92080abdf18355f7882c582c33797b
adityathodsare <thodsareaditya@gmail.com>
2025-09-13 08:16:34 +0530

    13/9 linkedlist 21

--- src/LinkedList/MargeTwoSortedLists21.java
+++ src/LinkedList/MargeTwoSortedLists21.java
@@ -1,5 +1,9 @@
 package LinkedList;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class MargeTwoSortedLists21 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null && list2 == null){
@@ -11,16 +15,52 @@ public class MargeTwoSortedLists21 {
         if (list2 ==null){
             return  list1;
         }
-        while( list2.next !=null){
-            if (list1.next.val > list2.val){
-                list1.next = list2;
-                continue;
-            }else{
-                list1 = list1.next;
-            }
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(list1 != null){
+            list.add(list1.val);
+            list1 =list1.next;
         }
-        return  list1;
+        while(list2 != null){
+            list.add(list2.val);
+            list2 =list2.next;
+        }
+        Collections.sort(list);
+        ListNode ans = new ListNode();
+        ListNode tail = ans ;
+
+        for (int val : list ){
+            tail.next = new ListNode(val);
+            tail = tail.next;
+        }
+
+        return  ans.next;
+
+
+    }
+
+
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode counter = dummy;
+        while(list1 != null && list2 != null ){
+            if (list1.val < list2.val){
+                counter.next = list1;
+                list1 = list1.next;
+            }else{
+                counter.next = list2 ;
+                list2 = list2.next;
+            }
+            counter = counter.next;
+        }
+
+        if (list1 != null){
+            counter.next = list1;
+        }else {
+            counter.next = list2;
+        }
+        return  dummy.next;
     }
 }
 
